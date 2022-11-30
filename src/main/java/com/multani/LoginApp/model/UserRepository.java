bbe8e3528a6fa25bbd9057e9e7bdb57d86080242
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-11-30 16:06:41 +0100

    Delete the query package

--- src/main/java/com/multani/LoginApp/model/UserRepository.java
+++ src/main/java/com/multani/LoginApp/model/UserRepository.java
@@ -4,7 +4,6 @@ package com.multani.LoginApp.model;
  */
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
