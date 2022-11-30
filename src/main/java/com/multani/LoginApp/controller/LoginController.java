b8c28d1e0bc6804870d0bd8fffbdd91018ff47ca
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-11-30 17:05:06 +0100

    Modify the login and RegisterController

---
+++ src/main/java/com/multani/LoginApp/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.multani.LoginApp.controller;/*
+ *author: Preet
+ */
+
+import com.multani.LoginApp.model.User;
+import com.multani.LoginApp.service.UserServiceIF;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+     private final UserServiceIF service;
+
+     @Autowired
+     public LoginController(UserServiceIF service) {
+         this.service = service;
+     }
+
+     @GetMapping("/getuser")
+     public User getUserData(@RequestBody User user) {
+         return service.getUser(user.getUsername());
+     }
+}
---
+++ src/main/java/com/multani/LoginApp/controller/RegisterController.java
@@ -0,0 +1,25 @@
+package com.multani.LoginApp.controller;/*
+ *author: Preet
+ */
+
+import com.multani.LoginApp.model.User;
+import com.multani.LoginApp.service.UserServiceIF;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/register")
+public class RegisterController {
+
+    private UserServiceIF service;
+
+    @Autowired
+    public RegisterController(UserServiceIF service) {
+        this.service = service;
+    }
+
+    @PostMapping("/userdata")
+    public void saveUserData(@RequestBody User user) {
+        service.saveNewUserData(user);
+    }
+}
