79d8baba8b68cfdc6dc4c7229c24d6647b060b7f
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-11-30 17:05:57 +0100

    Add two methods into the UserService interface

--- src/main/java/com/multani/LoginApp/service/UserServiceIF.java
+++ src/main/java/com/multani/LoginApp/service/UserServiceIF.java
@@ -2,5 +2,11 @@ package com.multani.LoginApp.service;/*
  *author: Preet
  */
 
+import com.multani.LoginApp.model.User;
+
 public interface UserServiceIF {
+
+    public void saveNewUserData(User user);
+
+    public User getUser(String username);
 }
