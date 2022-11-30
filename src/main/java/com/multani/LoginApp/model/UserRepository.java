14627b952f1bfcbf0d2125bb20007452d8ff1abd
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-11-30 16:14:22 +0100

    Add one method to get all data from a particular user

--- src/main/java/com/multani/LoginApp/model/UserRepository.java
+++ src/main/java/com/multani/LoginApp/model/UserRepository.java
@@ -4,9 +4,15 @@ package com.multani.LoginApp.model;
  */
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query(
+            value = "select * from user where username = ?1",
+            nativeQuery = true
+    )
+    User findUserByUsername(String username);
 }
