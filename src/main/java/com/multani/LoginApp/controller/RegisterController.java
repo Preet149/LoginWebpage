b9cb48686002a437b35b31433b0c701a9d98fb4d
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-12-09 16:52:36 +0100

    Made some slight changes

--- src/main/java/com/multani/LoginApp/controller/RegisterController.java
+++ src/main/java/com/multani/LoginApp/controller/RegisterController.java
@@ -26,11 +26,11 @@ public class RegisterController {
     }
 
     @GetMapping("/getuser_with_username")
-    public void getUserByUsername(@RequestBody String username) { service.getUserByUsername(username); }
+    public void getUserByUsername(@RequestBody User user) { service.getUserByUsername(user.getUsername()); }
 
     @GetMapping("/getuser_with_password")
-    public void getUserByPassword(@RequestBody String password) { service.getUserByPassword(password); }
+    public void getUserByPassword(@RequestBody User user) { service.getUserByPassword(user.getPassword()); }
 
     @GetMapping("/getuser_with_email")
-    public void getUserByEmail(@RequestBody String email) { service.getUserByEmail(email); }
+    public void getUserByEmail(@RequestBody User user) { service.getUserByEmail(user.getEmail()); }
 }
